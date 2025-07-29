61f7aa4e23342460c948ecb734dc9f7ab30358c2
Aman Badhautiya <167676131+amanjigithub@users.noreply.github.com>
2025-07-29 22:01:32 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import HireMe from "./components/HireMe";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/3d_portfolio/">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
