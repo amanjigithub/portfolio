2933554ba36b0a3f280aad1b8921e7d134c5b5ba
Aman Badhautiya <167676131+amanjigithub@users.noreply.github.com>
2025-07-29 22:39:29 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [{ find: "@", replacement: "/src" }],
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/3d_portfolio/", // This is crucial for GitHub Pages
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
